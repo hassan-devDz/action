c8a7b10c1a4002d05517d4ef80b09a24150994b0
hassan-devDz <hassan26102017@gmail.com>
2021-09-09 18:27:56 +0100

    📝 add cards

--- Components/static.js
+++ Components/static.js
@@ -3,50 +3,117 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import SchoolTwoToneIcon from '@material-ui/icons/SchoolTwoTone';
-import useStyle from '../Components/FormsUi/StyleForm';
+import SchoolTwoToneIcon from "@material-ui/icons/SchoolTwoTone";
+import useStyle from "../Components/FormsUi/StyleForm";
+import Image from "next/image";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import Kids from "../public/cards/kids_school.svg";
+import Icon from "@material-ui/core/Icon";
+import SchoolOutlinedIcon from "@material-ui/icons/SchoolOutlined";
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <Image src="/cards/kids_school.svg" alt="" layout="fill" />
+    </SvgIcon>
+  );
+}
+
 const Static = (prpos) => {
-  const classe = useStyle()
+  const classe = useStyle();
   const [sum, setsum] = useState(prpos.data);
   console.log(prpos.data);
-  const sumMohtaml = prpos.data.reduce(function (acc, val) {
+
+  const sumForced = prpos.data.reduce(function (acc, val) {
     return acc + val.forced;
   }, 0);
-  return (
-    <Grid container spacing={1} style={{margin:"6px 0"}}>
-      <Grid item xs={2}>
-        <Paper elevation={3} className={classe.buttonPaper}>
-          <Grid container  wrap="nowrap">
-            <Grid item xs={8} style={{padding:"5px 22px 0 22px"}}>
-              <Typography
-                variant="h4"
-                component="h4"
-                align="left"
-                gutterBottom
-                color="secondary"
-              >
-                {prpos.data.length}
-              </Typography>
-              <Typography
+ const arrItem = [
+  {key:"potentialVacancy",name:"محتمل الشغور",src:"/cards/mohtamal.svg"},
+  {key:"forced",name:"مجبر",src:"/cards/forced.svg"},
+  {key:"vacancy",name:"شاغر",src:"/cards/vacant.svg"},
+  {key:"surplus",name:"فائض",src:"/cards/Quitting-job-bro.svg"}]
+  const DataMap = ()=>arrItem.map((x)=>{
+    const sumForced = prpos.data.reduce(function (acc, val) {
+      return acc + val[x.key];
+    }, 0)
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={2} key={x.name} >
+      <Paper elevation={3} className={classe.buttonPaper}>
+        <Grid container wrap="nowrap">
+          <Grid item container xs={8} style={{ padding: "5px 22px 0 22px" }} justifyContent="space-around" direction="column">
+            <Typography
+              variant="body1"
+              component="h4"
+              align="left"
+              gutterBottom
+              color="textSecondary"
+              style={{ fontWeight: 700 ,whiteSpace:"nowrap"}}
+            >
+              {x.name}
+            </Typography><Typography
+              variant="h4"
+              component="h4"
+              align="left"
               
+              color="secondary"
+              style={{ fontFamily: "Helvetica", fontWeight: 700 }}
+            >
+              {sumForced}
+            </Typography>
+            
+          </Grid>
+          <Grid item xs={4} style={{ backgroundImage:`url(${x.src})`,backgroundPosition:"center",backgroundRepeat:'no-repeat',backgroundSize:"cover"}}>
+            
+            {/* <Icon>
+              <Image width={97} height={97} src={x.src} />
+            </Icon> */}
+            {/* <SchoolTwoToneIcon fontSize="large" style={{width:"100%",height:"100%",padding:10}} color="secondary"/> */}
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>)
+  })
+  console.log();
+ 
+  return (
+    <Grid container spacing={1} style={{ margin: "6px 0" }} >
+      
+      <Grid item xs={12} sm={6} md={4} lg={2} >
+        <Paper elevation={3} className={classe.buttonPaper}>
+          <Grid container wrap="nowrap">
+            <Grid item container xs={8} style={{ padding: "5px 22px 0 22px" }} justifyContent="space-around" direction="column">
+              <Typography
                 variant="h6"
                 component="h4"
                 align="left"
                 gutterBottom
                 color="secondary"
+                style={{ fontWeight: 700 }}
               >
                 عدد المدارس
+              </Typography><Typography
+                variant="h4"
+                component="h4"
+                align="left"
+                
+                color="secondary"
+                style={{ fontFamily: "Helvetica", fontWeight: 700 }}
+              >
+                {prpos.data.length}
               </Typography>
+              
             </Grid>
             <Grid item xs={4}>
-             <SchoolTwoToneIcon fontSize="large" style={{width:"100%",height:"100%",padding:10}} color="secondary"/>
+              <Icon>
+                <Image width={97} height={97} src="/cards/Eco_education-bro.svg" />
+              </Icon>
+              {/* <SchoolTwoToneIcon fontSize="large" style={{width:"100%",height:"100%",padding:10}} color="secondary"/> */}
             </Grid>
-         
           </Grid>
         </Paper>
       </Grid>
+     <DataMap/>
       
-    </Grid>
+   </Grid>
   );
 };
 export default Static;
